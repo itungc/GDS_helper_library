52ede99e5c72b777eeb683f447eba418930a8d4b
arripin <84411134+arripin@users.noreply.github.com>
2021-05-25 12:48:42 -0700

    Update ufIDT20210525.py

--- ufIDT20210525.py
+++ ufIDT20210525.py
@@ -1,3 +1,5 @@
+# array of single finger and double finger IDTs with wavlength 4um set to create standing waves. Example of how to make array of standing wave unfocused IDTs
+
 def double_finger_idt_wp(fw, frequency, wavelength, periods, height, surface_velocity, offset, finger_layer,
                       pad_layer, align_layer, coords, label):
     # f_or_w should be entered as 0 for f or 1 for w, depending on whether you would like to specify wavelength or frequency
